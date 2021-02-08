90817ba05128d378c09cdb80b5fe0502f9538446
donbasta <faddilafarras@gmail.com>
2021-02-09 00:26:12 +0700

    separate square from board

--- danau-danau/src/components/Board.js
+++ danau-danau/src/components/Board.js
@@ -1,51 +1,6 @@
 import React from 'react';
+import Square from './Square.js';
 import '../styles/Board.css';
-
-function Square(props) {
-    switch(props.value) {
-        case 'W':
-            return (
-                <button 
-                    className="square water"
-                    onClick={props.onClick}
-                >
-                </button>
-            );
-        case 'P':
-            return (
-                <button 
-                    className="square protected"
-                    onClick={props.onClick}
-                >
-                </button>
-            );
-        case 0:
-            return (
-                <button
-                    className="square player-1"
-                    onClick={props.onClick}
-                >
-                </button>
-            )
-        case 1:
-            return (
-                <button
-                    className="square player-2"
-                    onClick={props.onClick}
-                >
-                </button>
-            )
-        default:
-            return (
-                <button 
-                    className="square"
-                    onClick={props.onClick}
-                >
-                    {props.value}
-                </button>
-            );
-    }
-}
 
 class Board extends React.Component {
     renderSquare(i, j) {
---
+++ danau-danau/src/components/Square.js
@@ -0,0 +1,49 @@
+import '../styles/Board.css';
+
+function Square(props) {
+    switch(props.value) {
+        case 'W':
+            return (
+                <button 
+                    className="square water"
+                    onClick={props.onClick}
+                >
+                </button>
+            );
+        case 'P':
+            return (
+                <button 
+                    className="square protected"
+                    onClick={props.onClick}
+                >
+                </button>
+            );
+        case 0:
+            return (
+                <button
+                    className="square player-1"
+                    onClick={props.onClick}
+                >
+                </button>
+            )
+        case 1:
+            return (
+                <button
+                    className="square player-2"
+                    onClick={props.onClick}
+                >
+                </button>
+            )
+        default:
+            return (
+                <button 
+                    className="square"
+                    onClick={props.onClick}
+                >
+                    {props.value}
+                </button>
+            );
+    }
+}
+
+export default Square;
