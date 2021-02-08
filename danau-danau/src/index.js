704f2542e9556ce9ce74851e32efde49fd24d5bb
donbasta <faddilafarras@gmail.com>
2021-02-08 23:41:07 +0700

    fix array issues

--- danau-danau/src/index.js
+++ danau-danau/src/index.js
@@ -169,10 +169,14 @@ function noMoreMoves(board, firstPlayer) {
     return false;
 }
 
-function generateBoard(rows = 10, cols = 10) {
-    const c1 = 0.05;
+function generateBoard(rows = 30, cols = 30) {
+    const c1 = 0.3;
     const c2 = 0.1;
-    let board = Array(rows).fill(Array(cols).fill(null));
+    const board = Array(rows);
+    for (let i = 0; i < board.length; i++) {
+        board[i] = new Array(cols).fill(null);
+    }
+    console.log(board);
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
             const rand = Math.random();
@@ -181,18 +185,19 @@ function generateBoard(rows = 10, cols = 10) {
             }
         }
     }
-    console.log("lul", board);
     const dx = [1, -1, 0, 0];
     const dy = [0, 0, 1, -1];
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
-            if (!board[i][j]) {
+            if (board[i][j] == null) {
                 let flag = false;
                 for (let k = 0; k < 4; k++) {
                     const ni = i + dx[k];
                     const nj = j + dy[k];
                     if (ni >= 0 && ni < rows && nj >= 0 && nj < cols) {
-                        flag |= (board[i][j] === 'W');
+                        if (board[ni][nj] === 'W') {
+                            flag = true;
+                        }
                     }
                 }
                 if (flag) {
@@ -204,7 +209,6 @@ function generateBoard(rows = 10, cols = 10) {
             }
         }
     }
-    console.log("boardo", board)
     return board;
 }
 
