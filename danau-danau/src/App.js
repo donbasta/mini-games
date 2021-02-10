c8ffb7b5669fcbc0ad02ebb5f3d343c8023805d8
donbasta <faddilafarras@gmail.com>
2021-02-10 21:23:31 +0700

    setup basic routing

--- danau-danau/src/App.js
+++ danau-danau/src/App.js
@@ -1,9 +1,29 @@
 import Game from './components/Game.js';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Game />
+    <div className="container">
+      <h1>
+        danau-danau
+      </h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <div>
+              <nav>
+                <Link to="/play">Play!</Link>
+              </nav>
+            </div>
+          </Route>
+          <Route exact path="/play">
+            <div className="game-wrapper">
+              <Game />
+            </div>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
